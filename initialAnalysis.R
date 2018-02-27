9dccf41b8d37e65c393f87b077b436154a0021c4
Jeffrey Huskey <jhuskey@silasg.com>
2018-02-27 11:49:46 -0500

    now using read_csv() instead of read.csv() to import

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -4,34 +4,37 @@
 
 # Import ----
 setwd("~/marchMadness")
+library(tidyverse)
 
-cities <- read.csv(file = "~/march/Data/Cities.csv")
-conferences <- read.csv(file = "~/march/Data/Conferences.csv")
-conferenceTourneyGames <- read.csv(file = "~/march/Data/ConferenceTourneyGames.csv")
-events2010 <- read.csv(file = "~/march/Data/Events_2010.csv")
-#events2011 <- read.csv(file = "~/march/Data/Events_2011.csv") #commenting out until i'm sure i'll use since the files are huge, skipping the rest of the 'events' files for now
+cities <- read_csv(file = "~/march/Data/Cities.csv")
+conferences <- read_csv(file = "~/march/Data/Conferences.csv")
+conferenceTourneyGames <- read_csv(file = "~/march/Data/ConferenceTourneyGames.csv")
+events2010 <- read_csv(file = "~/march/Data/Events_2010.csv")
+#events2011 <- read_csv(file = "~/march/Data/Events_2011.csv") #commenting out until i'm sure i'll use since the files are huge, skipping the rest of the 'events' files for now
 
-gameCities <- read.csv(file = "~/march/Data/GameCities.csv")
-massey <- read.csv(file = "~/march/Data/MasseyOrdinals.csv")
-NCAATourneyCompactResults <- read.csv(file = "~/march/Data/NCAATourneyCompactResults.csv")
-NCAATourneyDetailedResults <- read.csv(file = "~/march/Data/NCAATourneyDetailedResults.csv")
-NCAATourneySeedRoundSlots <- read.csv(file = "~/march/Data/NCAATourneySeedRoundSlots.csv")
-NCAATourneySeeds <- read.csv(file = "~/march/Data/NCAATourneySeeds.csv")
-NCAATourneySlots <- read.csv(file = "~/march/Data/NCAATourneySlots.csv")
-players2010 <- read.csv(file = "~/march/Data/Players_2010.csv")
-#players2011 <- read.csv(file = "~/march/Data/Players_2011.csv")
+gameCities <- read_csv(file = "~/march/Data/GameCities.csv")
+massey <- read_csv(file = "~/march/Data/MasseyOrdinals.csv")
+NCAATourneyCompactResults <- read_csv(file = "~/march/Data/NCAATourneyCompactResults.csv")
+NCAATourneyDetailedResults <- read_csv(file = "~/march/Data/NCAATourneyDetailedResults.csv")
+NCAATourneySeedRoundSlots <- read_csv(file = "~/march/Data/NCAATourneySeedRoundSlots.csv")
+NCAATourneySeeds <- read_csv(file = "~/march/Data/NCAATourneySeeds.csv")
+NCAATourneySlots <- read_csv(file = "~/march/Data/NCAATourneySlots.csv")
+players2010 <- read_csv(file = "~/march/Data/Players_2010.csv")
+#players2011 <- read_csv(file = "~/march/Data/Players_2011.csv")
 
-regularSeasonCompactResults <- read.csv(file = "~/march/Data/RegularSeasonCompactResults.csv")
-regularSeasonDetailedResults <- read.csv(file = "~/march/Data/RegularSeasonDetailedResults.csv")
-seasons <- read.csv(file = "~/march/Data/Seasons.csv")
-secondaryTourneyCompactResults <- read.csv(file = "~/march/Data/SecondaryTourneyCompactResults.csv")
-secondaryTourneyTeams <- read.csv(file = "~/march/Data/SecondaryTourneyTeams.csv")
-teamCoaches <- read.csv(file = "~/march/Data/TeamCoaches.csv")
-teamConferences <- read.csv(file = "~/march/Data/TeamConferences.csv")
-teams <- read.csv(file = "~/march/Data/Teams.csv")
-teamSpellings <- read.csv(file = "~/march/Data/TeamSpellings.csv")
+regularSeasonCompactResults <- read_csv(file = "~/march/Data/RegularSeasonCompactResults.csv")
+regularSeasonDetailedResults <- read_csv(file = "~/march/Data/RegularSeasonDetailedResults.csv")
+seasons <- read_csv(file = "~/march/Data/Seasons.csv")
+secondaryTourneyCompactResults <- read_csv(file = "~/march/Data/SecondaryTourneyCompactResults.csv")
+secondaryTourneyTeams <- read_csv(file = "~/march/Data/SecondaryTourneyTeams.csv")
+teamCoaches <- read_csv(file = "~/march/Data/TeamCoaches.csv")
+teamConferences <- read_csv(file = "~/march/Data/TeamConferences.csv")
+teams <- read_csv(file = "~/march/Data/Teams.csv")
+teamSpellings <- read_csv(file = "~/march/Data/TeamSpellings.csv")
 
 # Tidy ----
 summary(massey)
-summary(regularSeasonCompactResults)
+summary(regularSeasonCompactResults) # aggregate the season data for each team
 summary(regularSeasonDetailedResults)
+summary(secondaryTourneyCompactResults) # could use as another source for model optimization - you wouldn't have these results to use ot predict
+summary(NCAATourneyCompactResults)
